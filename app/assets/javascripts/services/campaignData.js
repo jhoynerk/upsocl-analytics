0636af476a9b27798269096eb77362246d230627
Jhoynerk Caraballo <jhoynerk@gmail.com>
2016-08-17 12:43:51 -0400

    add model tags Angular JS

--- app/assets/javascripts/services/campaignData.js
+++ app/assets/javascripts/services/campaignData.js
@@ -2,6 +2,8 @@ myApp = angular.module('upsocl.services', [])
 
 myApp.factory('Campaign', function($resource) {
   return $resource('/campaigns/:id.json');
+}).factory('Tags', function($resource) {
+  return $resource('/tags.json');
 }).factory('CampaignFull', function($resource) {
   return $resource('/campaigns_full.json');
 }).factory('User', function($resource) {
