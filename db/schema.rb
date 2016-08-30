c6384e2c15f0fb5e9cb3783e43cf12e436ae95f5
Jhoynerk Caraballo <jhoynerk@gmail.com>
2016-08-30 19:44:47 -0400

    add model message

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160811131053) do
+ActiveRecord::Schema.define(version: 20160830201107) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -146,6 +146,14 @@ ActiveRecord::Schema.define(version: 20160811131053) do
 
   add_index "facebook_posts", ["facebook_account_id"], name: "index_facebook_posts_on_facebook_account_id", using: :btree
   add_index "facebook_posts", ["url_id"], name: "index_facebook_posts_on_url_id", using: :btree
+
+  create_table "messages", force: :cascade do |t|
+    t.string   "message",                 null: false
+    t.integer  "status",      default: 0, null: false
+    t.integer  "type_update", default: 0, null: false
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
+  end
 
   create_table "page_stadistics", force: :cascade do |t|
     t.integer  "url_id"
