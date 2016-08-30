0e40c5107f5ae953be20405fc2aca564a87f628a
Jhoynerk Caraballo <jhoynerk@gmail.com>
2016-08-30 19:43:18 -0400

    add info dashboard

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -10,6 +10,51 @@ ActiveAdmin.register_page "Dashboard" do
       end
     end
 
+    columns do
+      column do
+        panel "Ultimos 15 Mensajes de Actualización del Servidor Analytics" do
+          ul do
+            Message.where(type_update: 1).order(:id => :asc).limit(15).reverse.map do |message|
+              case message.status
+              when 1
+                li class: 'flash flash_warning' do
+                  message.message
+                end
+              when 2
+                li class: 'flash flash_notice' do
+                  message.message
+                end
+              when 3
+                li class: 'flash flash_error' do
+                  message.message
+                end
+              end
+            end
+          end
+        end
+        panel "Ultimos 15 Mensajes de Actualización del Servidor Facebook" do
+          ul do
+            Message.where(type_update: 2).order(:id => :asc).limit(15).reverse.map do |message|
+              case message.status
+              when 1
+                li class: 'flash flash_warning' do
+                  message.message
+                end
+              when 2
+                li class: 'flash flash_notice' do
+                  message.message
+                end
+              when 3
+                li class: 'flash flash_error' do
+                  message.message
+                end
+              end
+            end
+          end
+        end
+      end
+    end
+
     # Here is an example of a simple dashboard with columns and panels.
     #
     # columns do
