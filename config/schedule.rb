6d2ac355acac831f422bfb5d2f1bc31e80c8a833
Jhoynerk Caraballo <jhoynerk@gmail.com>
2016-08-30 20:28:34 -0400

    update schedule

--- config/schedule.rb
+++ config/schedule.rb
@@ -8,15 +8,15 @@
 set :environment, "production"
 set :output, "log/cron_log.log"
 
-every 1.day, :at => '10:00 am' do
+every '* 10 * * *' do
   rake "analytics:add_records[week, day]"
 end
 
-every 1.day, :at => '10:00 am' do
+every '0 0 1 * *' do
   rake "analytics:add_records[week, month]"
 end
 
-every 1.day, :at => '10:00 am' do
+every '0 0 1 1,7 *' do
   rake "analytics:add_records[week, 6month]"
 end
 
