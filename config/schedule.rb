6f3b300180303bf71f2933c29400a8441f375edc
Lenin Luque <xleninx@gmail.com>
2016-09-05 13:05:19 -0300

    clean

--- config/schedule.rb
+++ config/schedule.rb
@@ -8,18 +8,6 @@
 set :environment, "production"
 set :output, "log/cron_log.log"
 
-every '* 10 * * *' do
-  rake '"analytics:add_records[week, day]"'
-end
-
-every '0 0 1 * *' do
-  rake '"analytics:add_records[week, month]"'
-end
-
-every '0 0 1 1,7 *' do
-  rake '"analytics:add_records[week, 6month]"'
-end
-
 
 every 1.day, :at => '2:00 am' do
   rake "facebook:add_data"
