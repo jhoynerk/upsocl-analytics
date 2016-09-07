0a548ed508946920e2b57aee6048d080dc16de3e
Jhoynerk Caraballo <jhoynerk@gmail.com>
2016-09-07 16:40:47 -0400

    change validate password on create or update

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,14 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable
 
   has_and_belongs_to_many :campaigns
 
   validates :name, presence: true
+  validates :password, :password_confirmation, presence: true, on: :create
+
+  validates_presence_of :password_confirmation, if: -> { password.present? }
 
   def join_campaigns
     campaigns.map(&:name).join(', ')
