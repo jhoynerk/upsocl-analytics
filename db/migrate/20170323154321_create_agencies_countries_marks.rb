a2d7f71aec3260c9b9df02678c6c07d2e8bfb538
Jhoynerk Caraballo <jhoynerk@gmail.com>
2017-04-04 13:52:04 -0300

    update migrate

--- db/migrate/20170323154321_create_agencies_countries_marks.rb
+++ db/migrate/20170323154321_create_agencies_countries_marks.rb
@@ -2,7 +2,7 @@ class CreateAgenciesCountriesMarks < ActiveRecord::Migration
   def change
     create_table :agencies_countries_marks do |t|
       t.belongs_to :countries_mark, index: true
-      t.belongs_to :agencies, index: true
+      t.belongs_to :agency, index: true
     end
   end
 end
