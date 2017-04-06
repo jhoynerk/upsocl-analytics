5ebfa6687ada53b4752dd6ede76eec202554c844
Jhoynerk Caraballo <jhoynerk@gmail.com>
2017-04-06 10:40:04 -0300

    Update relationship
    Add Direct access to other models

--- app/models/agencies_countries_mark.rb
+++ app/models/agencies_countries_mark.rb
@@ -1,10 +1,14 @@
 class AgenciesCountriesMark < ActiveRecord::Base
   has_many :campaigns
   belongs_to :agency
-
+  belongs_to :countries_mark, dependent: :delete
   accepts_nested_attributes_for :campaigns
 
   def agency_name
     agency.name
+  end
+
+  def full_info
+    "#{countries_mark.mark_name} -> #{countries_mark.country_name} -> #{agency_name}"
   end
 end
--- app/models/countries_mark.rb
+++ app/models/countries_mark.rb
@@ -1,11 +1,15 @@
 class CountriesMark < ActiveRecord::Base
-  belongs_to :marks
+  belongs_to :mark
   belongs_to :country
-  has_many :agencies_countries_marks, :join_table => :agencies_countries_marks
+  has_many :agencies_countries_marks, :join_table => :agencies_countries_marks, dependent: :delete_all
 
   accepts_nested_attributes_for :agencies_countries_marks, allow_destroy: :true
 
-  def campaign_name
-    country.name
+  def country_name
+    country.name unless mark.nil?
+  end
+
+  def mark_name
+    mark.name unless mark.nil?
   end
 end
