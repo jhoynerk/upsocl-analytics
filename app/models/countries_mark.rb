28ae84268ecd6e5fac799b48f44679a1c3482609
Jhoynerk Caraballo <jhoynerk@gmail.com>
2017-04-13 10:27:15 -0300

    fix destroy childs

--- app/models/countries_mark.rb
+++ app/models/countries_mark.rb
@@ -1,13 +1,9 @@
 class CountriesMark < ActiveRecord::Base
   belongs_to :mark
   belongs_to :country
-  has_many :agencies_countries_marks, :join_table => :agencies_countries_marks, dependent: :delete_all
+  has_many :agencies_countries_marks, :join_table => :agencies_countries_marks, dependent: :destroy
 
   accepts_nested_attributes_for :agencies_countries_marks, allow_destroy: :true
-
-  before_destroy :destroy_all_agencies_countries_marks
-
-
 
   def country_name
     country.name unless country.nil?
@@ -21,8 +17,4 @@ class CountriesMark < ActiveRecord::Base
     "#{mark_name} #{country_name}"
   end
 
-  private
-  def destroy_all_agencies_countries_marks
-    agencies_countries_marks.delete_all if agencies_countries_marks.any?
-  end
 end
