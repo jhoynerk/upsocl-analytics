0c7c9c48f967354a6903277119b1e89229611bc5
Jhoynerk Caraballo <jhoynerk@gmail.com>
2017-04-17 11:53:52 -0300

    Add Validation Message

--- app/models/countries_mark.rb
+++ app/models/countries_mark.rb
@@ -1,9 +1,9 @@
 class CountriesMark < ActiveRecord::Base
   belongs_to :mark
   belongs_to :country
-  has_many :agencies_countries_marks, :join_table => :agencies_countries_marks, dependent: :destroy
+  has_many :agencies_countries_marks, join_table: :agencies_countries_marks, dependent: :destroy
 
-  validates_presence_of :agencies_countries_marks
+  validates_presence_of :agencies_countries_marks, message: "El país debe tener agencia, no puede estar en blanco"
   accepts_nested_attributes_for :agencies_countries_marks, allow_destroy: :true
 
   def country_name
