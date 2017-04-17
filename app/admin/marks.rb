a3af1c6cc2374bd2c19b734eb6aa5b7572e53475
Jhoynerk Caraballo <jhoynerk@gmail.com>
2017-04-17 13:47:47 -0300

    autoload agencies

--- app/admin/marks.rb
+++ app/admin/marks.rb
@@ -1,8 +1,6 @@
 ActiveAdmin.register Mark do
   permit_params :name, countries_marks_attributes: [ :id, :country_id, :mark_id, :_destroy, agencies_countries_marks_attributes: [ :id, :agency_id, :countries_mark_id, :_destroy, campaign_ids: [] ] ]
   menu parent: "Utilidad"
-  agencies = Agency.all.select(:name, :id )
-  campaigns = Campaign.all.select(:name, :id)
 
   filter :name
   filter :countries, label: 'Países', as: :select, collection: proc { Country.in_mark }, input_html: { class: 'chosen-input select_search'}
@@ -30,8 +28,8 @@ ActiveAdmin.register Mark do
       f.has_many :countries_marks, heading: 'Países', allow_destroy: true, new_record: 'Añadir País' do |a|
         a.input :country, :as => :select, :input_html => { :class => "chosen-input"}, label: "País"
         a.has_many :agencies_countries_marks, heading: 'Agencias', allow_destroy: true, new_record: 'Añadir Agencia', class: 'agencies_countries_panel' do |b|
-          b.input :agency_id, label: 'Agencia', as: :select, collection: agencies, input_html: { class: 'chosen-input select_agencia'}
-          b.input :campaign_ids, label: 'Campaña', as: :select, collection: campaigns.map{|c| ["#{c.name} [#{c.id}]", c.id]} , input_html: { :multiple => true, class: 'chosen-input'}
+          b.input :agency_id, label: 'Agencia', as: :select, collection: Agency.all.select(:name, :id ), input_html: { class: 'chosen-input select_agencia'}
+          b.input :campaign_ids, label: 'Campaña', as: :select, collection: Campaign.all.select(:name, :id).map{|c| ["#{c.name} [#{c.id}]", c.id]} , input_html: { :multiple => true, class: 'chosen-input'}
         end
       end
     end
