a7874119095b9241ce760855d0dbbbc79d3036d9
Jhoynerk Caraballo <jhoynerk@gmail.com>
2017-04-18 18:20:27 -0300

    Do not delete agency when you have client

--- app/admin/agency.rb
+++ app/admin/agency.rb
@@ -2,4 +2,15 @@ ActiveAdmin.register Agency do
   permit_params :name
   menu parent: "Utilidad"
   filter :name
+
+  controller do
+    def destroy
+      destroy! do |success, failure|
+        failure.html do
+          flash[:error] = "Mensaje: " + resource.errors.full_messages.to_sentence
+          render action: :index
+        end
+      end
+    end
+  end
 end
