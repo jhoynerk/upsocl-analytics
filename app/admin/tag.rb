9eb52334980a61f6e848ea03a9cfbd7a67c68def
daniel castillo <danielcastillo@MacBook-Pro-de-daniel.local>
2017-04-24 09:28:13 -0300

    show type filter humanize

--- app/admin/tag.rb
+++ app/admin/tag.rb
@@ -2,6 +2,19 @@ ActiveAdmin.register Tag do
   menu parent: "Ajustes"
   permit_params :title, :type_tag
 
+  filter :title
+  filter :type_tag, label: 'Tipo', as: :select, collection: proc { TagType.to_a }, input_html: { :multiple => true, class: 'chosen-input select_search'}
+
+  index do
+    selectable_column
+    column :id
+    column :title
+    column(:type_tag) do |u|
+      u.type_tag_humanize
+    end
+    actions
+  end
+
   form do |f|
     f.semantic_errors *f.object.errors.keys
     f.inputs "Etiquetas" do
