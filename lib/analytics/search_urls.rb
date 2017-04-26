418f7b95bd54e4b5a29af01f1c6f83ae9cf8b5f1
Jhoynerk Caraballo <jhoynerk@gmail.com>
2017-04-26 11:24:07 -0300

    add class search urls.

---
+++ lib/analytics/search_urls.rb
@@ -0,0 +1,26 @@
+class SearchUrls
+  def initialize(url_id = nil)
+    @url_id = url_id
+  end
+
+  def searh
+    search_urls
+  end
+
+  private
+    def search_urls
+      (@url_id.nil?) ? search_all_urls : search_url
+    end
+
+    def search_url
+      [ Url.where(id: @url_id) ]
+    end
+
+    def search_all_urls
+      Url.search_urls_to_update
+    end
+
+    def interval_range
+      IntervalRange.new(args.interval)
+    end
+end
