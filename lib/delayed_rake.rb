c4dbf3d46ac74ed433ea43a2374f75b93a61eac5
upsocl <francisco.herrera@upsocl.com>
2017-04-27 09:25:18 -0300

    delayed rake out

--- lib/delayed_rake.rb
+++
@@ -1,9 +0,0 @@
-class DelayedRake < Struct.new(:task, :options)
-  def perform
-    env_options = ''
-    options && options.stringify_keys!.each do |key, value|
-      env_options << " #{key.upcase}=#{value}" if key
-    end
-    system("cd #{Rails.root} && RAILS_ENV=#{Rails.env} bundle exec rake #{task} #{env_options} >> log/delayed_rake.log")
-  end
-end
