aaa0149754fdd63320b23f108b92ff464a02acf6
Jhoynerk Caraballo <jhoynerk@gmail.com>
2017-04-28 13:08:46 -0300

    add constants or initialize for the date attribute

--- lib/analytics/update_analytic.rb
+++ lib/analytics/update_analytic.rb
@@ -1,10 +1,12 @@
 class UpdateAnalytic
+  START_DATE = 3.month.ago
+  END_DATE = 1.day.ago
 
-  def initialize(url, start_date, end_date)
+  def initialize(url, start_date = nil, end_date = nil)
     @stadistics = ['PageStadistic', 'CountryStadistic', 'TrafficStadistic', 'DeviceStadistic']
     @url = url
     @start_date = start_date
-    @end_date = end_date
+    @end_date = start_date
     search_data
   end
 
@@ -15,6 +17,14 @@ class UpdateAnalytic
   end
 
   private
+    def attr_start_date
+      @start_date ||= UpdateAnalytic::START_DATE
+    end
+
+    def attr_end_date
+      @end_date ||= UpdateAnalytic::END_DATE
+    end
+
     def data(model)
       conection(@url.profile_id, source(model), @url.only_path)
     end
@@ -24,6 +34,6 @@ class UpdateAnalytic
     end
 
     def conection(profile, source, path)
-      AnalyticConnection.new(profile).historical_data_for(source: source, url: path, start_date: @start_date, end_date: @end_date)
+      AnalyticConnection.new(profile).historical_data_for(source: source, url: path, start_date: attr_start_date, end_date: attr_end_date)
     end
 end
