6ea052e95939d400efc4fa21e6733e38b8ef0c8d
daniel castillo <danielcastillo@MacBook-Pro-de-daniel.local>
2017-05-02 16:36:11 -0300

    clean file of comments

--- app/models/facebook_connection.rb
+++ app/models/facebook_connection.rb
@@ -65,30 +65,3 @@ class FacebookConnection
       @oauth_access_token = ENV['FB_ACCESS_TOKEN'];
     end
 end
-
-# post_impressions_unique, #personas alcanzadas
-# post_video_avg_time_watched, #tiempo promedio
-# post_video_views # reproducciones de video
-# post_video_view_time #Minutos totales reproducidos /1000/60
-# @graph.get_object(user_post, fields: "insights.metric(post_impressions_unique, post_video_views, post_video_avg_time_watched, post_video_view_time).period(lifetime){name,values}, reactions.summary(true).limit(0), comments.summary(true).filter(stream).limit(0), shares")
-
-  # def test
-  #   # text graph 219769364857951_825943334240548?fields=shares,reactions.summary(true)
-  #   # text graph 219769364857951_674076472760569/video_insights
-  #   @user_id = '219769364857951'
-  #   @post_id = '674076472760569'
-  #   #upsoclsabores = '1922865171271266'
-  #   parameters = {
-  #   fields: ['insights' ]
-  #   }
-  #   #object(user_post, "video")
-  #   @graph.get_object(user_post, parameters)
-  #   #@graph.get_object(user_post, fields: 'video_lists' )
-  #   #object(user_post, "video_insights")
-
-  #   #FacebookConnection.new(219769364857951, 674076472760569).test
-  # end
-
-  #@oauth_access_token ||= Koala::Facebook::OAuth.new(ENV['FB_APP_ID'], ENV['FB_APP_SECRET']).get_app_access_token
-  # get_data['insights']['data'].select{|metric| metric['name'] == 'post_impressions_unique'}.first['values'].first['value']
-
