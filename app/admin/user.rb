956de75415d59bc8f9657f2a534e93c9482b0ff2
Lenin Luque <xleninx@gmail.com>
2017-05-03 15:22:08 -0300

    add missing attribute

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register User do
-  permit_params :email, :name, :admin,:password, :password_confirmation, campaign_ids: []
+  permit_params :email, :name, :admin,:password, :password_confirmation, :role, campaign_ids: []
   show do
     panel 'Detalles de Usuario' do
       attributes_table_for user do
