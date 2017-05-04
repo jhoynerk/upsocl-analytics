89eb77ecf11b75544a3706dee849e550c9258466
Jhoynerk Caraballo <jhoynerk@gmail.com>
2017-05-04 18:38:49 -0300

    add url inactive and url active not dfp

--- spec/tasks/analytics_update_spec.rb
+++ spec/tasks/analytics_update_spec.rb
@@ -7,6 +7,8 @@ describe "analytics:update" do
     let!(:url_branded) { create(:url, status: true, data: 'http://www.upsocl.com/branded/10-looks-para-que-te-atrevas-a-usar-la-tendencia-que-se-tomo-instagram-pestanas-de-colores/', line_id: 437733004) }
     let!(:url_active_week) { create(:url, status: true, data: 'http://www.upsocl.com/branded/10-looks-para-que-te-atrevas-a-usar-la-tendencia-que-se-tomo-instagram-pestanas-de-colores/', line_id: 437733004, data_updated_at: 8.days.ago, created_at: 8.days.ago) }
     let!(:url_active_month) { create(:url, status: true, data: 'http://www.upsocl.com/branded/10-looks-para-que-te-atrevas-a-usar-la-tendencia-que-se-tomo-instagram-pestanas-de-colores/', line_id: 437733004, data_updated_at: 32.days.ago, created_at: 32.days.ago) }
+    let!(:url_inactive) { create(:url) }
+    let!(:url_active_not_dfp) { create(:url, status: true, data: 'http://www.upsocl.com/branded/10-looks-para-que-te-atrevas-a-usar-la-tendencia-que-se-tomo-instagram-pestanas-de-colores/', line_id: 0) }
     before{ subject.invoke }
 
 
@@ -36,6 +38,18 @@ describe "analytics:update" do
       expect(TrafficStadistic.where(url:  url_active_month).count).not_to eq(0)
       expect(CountryStadistic.where(url:  url_active_month).count).not_to eq(0)
       expect(DfpStadistic.where(url:  url_active_month).count).not_to eq(0)
+
+      expect(PageStadistic.where(url:  url_inactive).count).to eq(0)
+      expect(DeviceStadistic.where(url:  url_inactive).count).to eq(0)
+      expect(TrafficStadistic.where(url:  url_inactive).count).to eq(0)
+      expect(CountryStadistic.where(url:  url_inactive).count).to eq(0)
+      expect(DfpStadistic.where(url:  url_inactive).count).to eq(0)
+
+      expect(PageStadistic.where(url:  url_active_not_dfp).count).not_to eq(0)
+      expect(DeviceStadistic.where(url:  url_active_not_dfp).count).not_to eq(0)
+      expect(TrafficStadistic.where(url:  url_active_not_dfp).count).not_to eq(0)
+      expect(CountryStadistic.where(url:  url_active_not_dfp).count).not_to eq(0)
+      expect(DfpStadistic.where(url:  url_active_not_dfp).count).to eq(0)
     end
 
   end
