e57e7c18cf2d98e4c873c45283a5d2907b7833ad
Jhoynerk Caraballo <jhoynerk@gmail.com>
2017-05-05 16:51:44 -0300

    schema crazy

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180421215339) do
+ActiveRecord::Schema.define(version: 20170505190548) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
