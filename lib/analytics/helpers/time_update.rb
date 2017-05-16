b0e0b31dd0fc9cbd7a875f328af1bc21c667f76e
Jhoynerk Caraballo <jhoynerk@gmail.com>
2017-05-16 19:29:52 -0300

    fix - date response

--- lib/analytics/helpers/time_update.rb
+++ lib/analytics/helpers/time_update.rb
@@ -1,10 +1,14 @@
 module TimeUpdate
 
-  START_DATE = 1.week.ago
-  END_DATE = 1.day.ago
+  START_DATE = 1.week.ago.to_date
+  END_DATE = 1.day.ago.to_date
 
   private
     def attr_start_date
+      @attr_start_date ||= get_start_date
+    end
+
+    def get_start_date
       lower_than_constant_date? ? @start_date : TimeUpdate::START_DATE
     end
 
