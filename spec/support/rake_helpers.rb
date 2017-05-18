f9e4b9f3bf2bd710b451f61c27b9e1d5cf060617
Jhoynerk Caraballo <jhoynerk@gmail.com>
2017-05-18 16:11:51 -0300

    fix test

--- spec/support/rake_helpers.rb
+++ spec/support/rake_helpers.rb
@@ -12,10 +12,10 @@ module RakeHelpers
 
   def is_update(args = {})
     let!(:url) { 'http://www.upsocl.com/branded/10-looks-para-que-te-atrevas-a-usar-la-tendencia-que-se-tomo-instagram-pestanas-de-colores/' }
-    let!(:line_id) { 437733004 }
+    let!(:line_id) { args[:line_id].nil? ? 437733004 : args[:line_id] } if
     let!(:url_test) { create(:url, data: url, line_id: line_id, publication_date: args[:start_date], publication_end_date: args[:end_date] ) }
     describe do
-      it 'should be' do
+      it message_test(args) do
         subject.invoke
         expect(PageStadistic.where(url:  url_test).count).to eq(args[:analytics])
         expect(DeviceStadistic.where(url:  url_test).count).to eq(args[:analytics])
@@ -25,5 +25,9 @@ module RakeHelpers
         count_messages_status(counter)
       end
     end
+  end
+
+  def message_test(args)
+    "the publication date is #{args[:start_date].to_date} and publication end date is #{args[:end_date].to_date}, today is #{Date.today} should have analytics #{args[:analytics]} and dfp #{args[:dfp]}"
   end
 end
--- spec/tasks/analytics_update_spec.rb
+++ spec/tasks/analytics_update_spec.rb
@@ -4,44 +4,41 @@ include RakeHelpers
 describe "analytics:update" do
   include_context "rake"
 
-  context 'When the article has already passed a week of its date to be able to update.' do
+  context 'When the article was published in ' do
     is_update({ start_date: 15.days.ago, end_date: 7.days.ago, analytics: 0, dfp: 0 })
+    is_update({ start_date: 2.month.ago, end_date: 1.month.ago, analytics: 0, dfp: 0 })
   end
 
-  context 'When the article has already passed a month of its date to be able to update.' do
-    is_update({ start_date: 2.month.ago, end_date: 30.days.ago, analytics: 0, dfp: 0 })
-  end
-
-  context 'When a url is published to be updated for 1 week' do
+  context 'When the article is published last week' do
     is_update({ start_date: 7.month.ago, end_date: 1.days.ago, analytics: 7, dfp: 7 })
   end
 
-  context 'When a url is published to be updated for 6 days' do
-    is_update({ start_date: 6.month.ago, end_date: 1.days.ago, analytics: 6, dfp: 6 })
+  context 'When the article is published the last 6 days' do
+    is_update({ start_date: 6.days.ago, end_date: 1.days.ago, analytics: 6, dfp: 6 })
   end
 
-  context 'When a url is published to be updated for 5 days' do
-    is_update({ start_date: 5.month.ago, end_date: 1.days.ago, analytics: 5, dfp: 5 })
+  context 'When the article is published the last 5 days' do
+    is_update({ start_date: 5.days.ago, end_date: 1.days.ago, analytics: 5, dfp: 5 })
   end
 
-  context 'When a url is published to be updated for 4 days' do
-    is_update({ start_date: 4.month.ago, end_date: 1.days.ago, analytics: 4, dfp: 4 })
+  context 'When the article is published the last 4 days' do
+    is_update({ start_date: 4.days.ago, end_date: 1.days.ago, analytics: 4, dfp: 4 })
   end
 
-  context 'When a url is published to be updated for 1 days' do
+  context 'When the article is published the last 1 days' do
     is_update({ start_date: 1.days.ago, end_date: 14.days.since, analytics: 1, dfp: 1 })
   end
 
-  context 'When the url is published in a week, it should not obtain metrics.' do
+  context 'When the article is published the following week' do
     is_update({ start_date: 7.days.since, end_date: 14.days.since, analytics: 0, dfp: 0 })
   end
 
-  context 'When the article is published today, it should not obtain metrics.' do
+  context 'When the article is published today' do
     is_update({ start_date: Date.today, end_date: 14.days.since, analytics: 0, dfp: 0 })
   end
 
-  context 'When a url is published to be updated analytics, but not DFP' do
-    #is_update({ start_date: 7.month.ago, end_date: 1.days.ago, analytics: 7, dfp: 0 })
+  context 'When the article is published last week But not' do
+    is_update({ start_date: 7.month.ago, end_date: 1.days.ago, analytics: 7, dfp: 0, line_id: 0 })
   end
 
 end
